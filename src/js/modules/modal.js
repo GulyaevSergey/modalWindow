853009df5ea2d2b7fc74012537caf45179c23c77
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2023-03-16 22:22:05 +0700

    removed folder dist

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,43 +1,56 @@
 const modals = () => {
     function bindModal(triggerSelector, modalSelector, closeSelector) {
-        const trigger = document.querySelectorAll(triggerSelector),
-              modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector);
+        const triggers = document.querySelectorAll(triggerSelector);
+        const modal = document.querySelector(modalSelector);
+        const close = document.querySelector(closeSelector);
 
-        trigger.forEach(item => {
-            item.addEventListener('click', (e) => {
+        const closeModal = () => {
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+        };
+
+        triggers.forEach((trigger) => {
+            trigger.addEventListener("click", (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
-    
+
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
             });
         });
 
-        close.addEventListener('click', () => {
-            modal.style.display = "none";
-            document.body.style.overflow = "";
+        close.addEventListener("click", () => {
+            closeModal();
         });
 
-        modal.addEventListener('click', (e) => {
+        modal.addEventListener("click", (e) => {
             if (e.target === modal) {
-                modal.style.display = "none";
-                document.body.style.overflow = ""; 
+                closeModal();
+            }
+        });
+
+        window.addEventListener("keyup", (e) => {
+            if (e.key === "Escape") {
+                closeModal();
             }
         });
     }
 
-    function showModalByTime(selector, time) {
-        setTimeout(function() {
-            document.querySelector(selector).style.display = 'block';
+    const showModalByTime = (selector, time) => {
+        setTimeout(function () {
+            document.querySelector(selector).style.display = "block";
             document.body.style.overflow = "hidden";
         }, time);
-    }
+    };
 
-    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-    bindModal('.phone_link', '.popup', '.popup .popup_close');
-    showModalByTime('.popup', 60000);
+    bindModal(
+        ".popup_engineer_btn",
+        ".popup_engineer",
+        ".popup_engineer .popup_close"
+    );
+    bindModal(".phone_link", ".popup", ".popup .popup_close");
+    showModalByTime(".popup", 60000);
 };
 
 export default modals;
