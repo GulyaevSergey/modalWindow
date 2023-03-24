f8ab080dd14237deb228743bc20e32ed25ad6b74
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2023-03-24 12:55:31 +0700

    added functionality to close the modal window, set up a quiz

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,8 +1,14 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(
+        triggerSelector,
+        modalSelector,
+        closeSelector,
+        closeClickOverlay = true
+    ) {
         const triggers = document.querySelectorAll(triggerSelector);
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector);
+        const windows = document.querySelectorAll("[data-modal]");
 
         const closeModal = () => {
             modal.style.display = "none";
@@ -14,7 +20,9 @@ const modals = () => {
                 if (e.target) {
                     e.preventDefault();
                 }
-
+                windows.forEach((window) => {
+                    window.style.display = "none";
+                });
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
 
@@ -23,17 +31,26 @@ const modals = () => {
         });
 
         close.addEventListener("click", () => {
+            windows.forEach((window) => {
+                window.style.display = "none";
+            });
             closeModal();
         });
 
         modal.addEventListener("click", (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach((window) => {
+                    window.style.display = "none";
+                });
                 closeModal();
             }
         });
 
         window.addEventListener("keyup", (e) => {
             if (e.key === "Escape") {
+                windows.forEach((window) => {
+                    window.style.display = "none";
+                });
                 closeModal();
             }
         });
@@ -53,6 +70,19 @@ const modals = () => {
     );
     bindModal(".phone_link", ".popup", ".popup .popup_close");
     bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+    bindModal(
+        ".popup_calc_button",
+        ".popup_calc_profile",
+        ".popup_calc_profile_close",
+        false
+    );
+    bindModal(
+        ".popup_calc_profile_button",
+        ".popup_calc_end",
+        ".popup_calc_end_close",
+        false
+    );
+
     showModalByTime(".popup", 60000);
 };
 
